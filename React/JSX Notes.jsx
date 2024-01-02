9d912d2312bba4189418428d0ccfd956ec302f35
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-01-02 18:41:28 -0500

    example of element saved to var

--- React/JSX Notes.jsx
+++ React/JSX Notes.jsx
@@ -3,3 +3,5 @@
 //     - syntax extension means not valid javascript (web browsers cant read it)
 //     - jsx compiler translates jsx into javascript before it reaches web browser
 
+// example of a JSX element being saved in a variable
+const navBar = <nav>I am a nav bar</nav>;
