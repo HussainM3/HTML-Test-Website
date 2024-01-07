9b405d7c97042c787c8b22776072b3952d89358d
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-01-07 18:51:05 -0500

    added another use of props

--- React/props/App.js
+++ React/props/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Product from './Product'
 
 function App() {
-  return <Product name="Apple Watch" price = {399} rating = "4.5/5.0" />;
+  return <Product name="Apple Watch" price = {399} rating = "4.5/5.0" isCheap = {false} />;
 }
 
 // can also call Product component
--- React/props/Product.js
+++ React/props/Product.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 function Product(props) {
+//   if (props.isCheap) // prop doesnt have to be string and displayed
   return (
     <div>
       <h1>{props.name}</h1>
       <h2>{props.price}</h2>
       <h3>{props.rating}</h3>
-    </div>       
+    </div>
   );
+//   else{}
 }
 
 export default Product;
